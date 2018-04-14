36d4f8e717376ece6a184a3e6c897768ab615253
bkfl <37470749+bkfl@users.noreply.github.com>
2018-04-15 09:39:45 +1000

    Update RPi_Morse_Code.py

--- RPi_Morse_Code.py
+++ RPi_Morse_Code.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from gpiozero import LED
+from time import sleep
 import RPi.GPIO
 
 RPi.GPIO.setmode(RPi.GPIO.BCM)
@@ -19,7 +20,9 @@ def blink():
     if (1 < len(input) < 13):
         # Check string contains letters only
         if (input.isalpha()):
-            
+            for c in input:
+                morseBlink(c)
+                morseCharRest()
         else:
             messagebox.showinfo("Error", "Input must contain only letters.")
     else:
@@ -31,13 +34,129 @@ def close():
 
 # FUNCTIONS
 def morseDot():
-    
+    led.on()
+    sleep(0.3)
+    led.off()
+    sleep(0.3)
+
 def morseDash():
+    led.on()
+    sleep(0.9)
+    led.off()
+    sleep(0.3)
 
 def morseCharRest():
-    
+    sleep(0.6)
 
 def morseBlink(char):
+    if (char == 'a'):
+        morseDot()
+        morseDash()
+    elif (char == 'b'):
+        morseDash()
+        morseDot()
+        morseDot()
+        morseDot()
+    elif (char == 'c'):
+        morseDash()
+        morseDot()
+        morseDash()
+        morseDot()
+    elif (char == 'd'):
+        morseDash()
+        morseDot()
+        morseDot()
+    elif (char == 'e'):
+        morseDot()
+    elif (char == 'f'):
+        morseDot()
+        morseDot()
+        morseDash()
+        morseDot()
+    elif (char == 'g'):
+        morseDash()
+        morseDash()
+        morseDot()
+    elif (char == 'h'):
+        morseDot()
+        morseDot()
+        morseDot()
+        morseDot()
+    elif (char == 'i'):
+        morseDot()
+        morseDot()
+    elif (char == 'j'):
+        morseDot()
+        morseDash()
+        morseDash()
+        morseDash()
+    elif (char == 'k'):
+        morseDash()
+        morseDot()
+        morseDash()
+    elif (char == 'l'):
+        morseDot()
+        morseDash()
+        morseDot()
+        morseDot()
+    elif (char == 'm'):
+        morseDash()
+        morseDash()
+    elif (char == 'n'):
+        morseDash()
+        morseDot()
+    elif (char == 'o'):
+        morseDash()
+        morseDash()
+        morseDash()
+    elif (char == 'p'):
+        morseDot()
+        morseDash()
+        morseDash()
+        morseDot()
+    elif (char == 'q'):
+        morseDash()
+        morseDash()
+        morseDot()
+        morseDash()
+    elif (char == 'r'):
+        morseDot()
+        morseDash()
+        morseDot()
+    elif (char == 's'):
+        morseDot()
+        morseDot()
+        morseDot()
+    elif (char == 't'):
+        morseDash()
+    elif (char == 'u'):
+        morseDot()
+        morseDot()
+        morseDash()
+    elif (char == 'v'):
+        morseDot()
+        morseDot()
+        morseDot()
+        morseDash()
+    elif (char == 'w'):
+        morseDot()
+        morseDash()
+        morseDash()
+    elif (char == 'x'):
+        morseDash()
+        morseDot()
+        morseDot()
+        morseDash()
+    elif (char == 'y'):
+        morseDash()
+        morseDot()
+        morseDash()
+        morseDash()
+    elif (char == 'z'):
+        morseDash()
+        morseDash()
+        morseDot()
+        morseDot()
 
 
 # WIDGETS
