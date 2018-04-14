13b48105977f6f23ddb63cd0a9269ebc39059fc2
bkfl <37470749+bkfl@users.noreply.github.com>
2018-04-15 09:07:10 +1000

    Create RPi_Morse_Code.py

---
+++ RPi_Morse_Code.py
@@ -0,0 +1,39 @@
+from Tkinter import *
+from gpiozero import LED
+import RPi.GPIO
+
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+
+# OUTPUTS
+led = LED(21)
+
+# GUI
+root = Tk()
+root.title("RPi: Morse Code")
+
+# EVENT FUNCTIONS
+def blink():
+    input = eInput.get()
+    # Check input is between 1 and 12 characters
+    if (1 <= len(input) >= 12):
+        # Check string contains letters only
+        if (input.isalpha()):
+            
+        else:
+            messagebox.showinfo("Error", "Input must contain only letters.")
+    else:
+        messagebox.showinfo("Error", "Input must be 1 to 12 characters.")
+
+def close():
+    RPi.GPIO.cleanup()
+    root.destroy()
+
+# WIDGETS
+lbl1 = Label(root, text="Enter a word (12 char max):")
+lbl1.pack()
+eInput = Entry(root)
+eInput.pack()
+btnBlink = Button(root, text="Blink It!", command=blink)
+btnBlink.pack()
+btnClose = Button(root, text="Close", command=close)
+btnClose.pack()
